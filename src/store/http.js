fb7dfb629f08498200fb863c6a547976694b7fe9
sakuara <845888325@qq.com>
2021-04-26 22:02:01 +0800

    20210426-rollback fro topic reducer

--- src/store/http.js
+++ src/store/http.js
@@ -4,10 +4,4 @@ const http =Axios.create({
     baseURL: 'https://cnodejs.org/api/v1'
 });
 
-const httpPre = Axios.create({
-    baseURL: 'https://cnodejs.org',
-    withCredentials: true
-})
-
 export default http;
-export {httpPre};
